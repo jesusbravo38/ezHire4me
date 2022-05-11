41c2c01b497b42bfd85b5a299c2788cd86ffe4f4
Jesus M. Bravo <jesusbravo38@gamil.com>
2022-05-11 23:42:02 +0000

    05-11-2022@16:40 - New changes to displayed phrase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to ezHire4me.com - Development Environment
+          Welcome to ezHire4me.com
         </p>
         <a
           className="App-link"
