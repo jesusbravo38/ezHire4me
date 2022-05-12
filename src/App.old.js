706597a5c028cc6a48426dfcbd83e836701cf42e
Jesús M. Bravo <jesusbravo38@gmail.com>
2022-05-11 17:43:24 -0700

    05-10-2022@17:44 - Change to App.js

--- src/App.old.js
+++ src/App.old.js
@@ -9,6 +9,14 @@ function App() {
         <p>
           Welcome to ezHire4me.com
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
