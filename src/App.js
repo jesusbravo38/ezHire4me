cba0b5aec3244b63f47aab5427118df1dc791e5e
Jesús M. Bravo <jesusbravo38@gmail.com>
2022-05-11 18:39:46 -0700

    05-11-2022@17:39 - New attempt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import logo from "./images/ezHire4me-dark.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/images/ezHire4me-dark.jpg" className="App-logo" alt="logo"/>
+        <img src={logo} className="ezHire4me-logo" alt="logo"/>
         <p>
           Welcome to ezHire4me.com
         </p>
