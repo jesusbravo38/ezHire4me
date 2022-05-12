625ecc0edea79271176ea8e139e47c88a1c649f2
Jesús M. Bravo <jesusbravo38@gmail.com>
2022-05-11 23:27:59 -0700

    05-11-2022@23:27 - Addition of under construction image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import logo from "./images/ezHire4me-dark.jpg";
+import image from "./images/under-construction.gif"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="ezHire4me-logo" alt="logo"/>
+        <img src={image} className="under-construction" alt="image"/>
         <p>
           Welcome to ezHire4me.com
         </p>
