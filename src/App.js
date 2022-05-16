5371d55e2944eeb1292eceae01eab9672d434820
Jesus Bravo <jesus.bravo@Jesuss-MBP.lan>
2022-05-16 16:07:49 -0700

    05-16-2022@16:06 | Latest changes to the login page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import SignUpForm from "./pages/SignUpForm";
+import SignInForm from "./pages/SignInForm";
 import logo from "./images/ezHire4me-dark.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="ezHire4me-logo" alt="logo"/>
-        <p>
-          Welcome to ezHire4me.com
-        </p>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="appAside">
+            <div className="parent">
+              <div className="container">
+                <img
+                  src={logo}
+                  className="ezHire4me-logo"
+                  alt="logo"
+                  width="1108"
+                  hight="2042 "
+                />
+              </div>
+            </div>
+          </div>
+          <div className="appForm">
+            <div className="pageSwitcher">
+              <NavLink
+                to="/sign-in"
+                activeClassName="pageSwitcherItem-active"
+                className="pageSwitcherItem"
+              >
+                Sign In
+              </NavLink>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="pageSwitcherItem-active"
+                className="pageSwitcherItem"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <div className="formTitle">
+              <NavLink
+                to="/sign-in"
+                activeClassName="formTitleLink-active"
+                className="formTitleLink"
+              >
+                Sign In
+              </NavLink>{" "}
+              or{" "}
+              <NavLink
+                exact
+                to="/"
+                activeClassName="formTitleLink-active"
+                className="formTitleLink"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <Route exact path="/" component={SignUpForm} />
+            <Route path="/sign-in" component={SignInForm} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
